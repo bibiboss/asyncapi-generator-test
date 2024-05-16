7f7c7734698a36b8572d011455d9c64ab67429c0
Brieuc Horard <brieuc.horard@non.se.com>
2024-05-16 17:22:19 +0200

    kickstart example

---
+++ asyncapi test/src/main/java/fr/horard/brieuc/test/AsyncapiTestApplication.java
@@ -0,0 +1,13 @@
+package fr.horard.brieuc.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncapiTestApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AsyncapiTestApplication.class, args);
+  }
+
+}
---
+++ asyncapi test/src/test/java/fr/horard/brieuc/test/AsyncapiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.horard.brieuc.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncapiTestApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
